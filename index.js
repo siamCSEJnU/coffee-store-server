346ca56fda7dab9bd453bf85a115ed0ac2d9bc3c
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-11 01:17:27 +0600

    coffee caught from client and posted to the database and also functionality of getting database executed

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || "5000";
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2jfyhqy.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
@@ -29,6 +29,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeCollection = client.db("coffeDB");
+    const coffee = coffeCollection.collection("coffee");
+
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffee.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffee.insertOne(newCoffee);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -36,7 +53,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
