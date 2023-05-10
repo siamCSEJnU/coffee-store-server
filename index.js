5283701223d7beadfe80e60399b973fa9b3dc467
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-05-11 02:13:27 +0600

    delete operation implemnented

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
 const port = process.env.PORT || "5000";
@@ -43,6 +43,13 @@ async function run() {
       const newCoffee = req.body;
       console.log(newCoffee);
       const result = await coffee.insertOne(newCoffee);
+      res.send(result);
+    });
+
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }; //kon property match kore paite chai
+      const result = await coffee.deleteOne(query);
       res.send(result);
     });
 
